88be56b17eddb456461f62b3f35084fa1030296e
maykcaldas <maykcaldas@gmail.com>
2023-04-12 18:10:59 -0400

    Brings the project from HuggingFace to GitHub

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
----
-title: MAPI LLM
-emoji: 👀
-colorFrom: gray
-colorTo: purple
-sdk: gradio
-sdk_version: 3.23.0
-app_file: app.py
-pinned: false
-license: mit
----
+# MAPI_LLM 🧱🦜️🔗
 
-Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+## A LLM application developed during the LLM March *MADNESS* Hackathon
+  - Developed by: 
+    - Mayk Caldas ([@maykcaldas](https://github.com/maykcaldas)), and 
+    - Sam Cox ([@SamCox822](https://github.com/SamCox822))
+
+### What is this?
+- This is a demo of an app that can answer questions about material science using the [LangChain🦜️🔗](https://github.com/hwchase17/langchain/) and the [Materials Project API](https://materialsproject.org/).
+- Its behavior is based on Large Language Models (LLM), and it aims to be a tool to help scientists with quick predictions of numerous properties of materials.
+
+>It is a work in progress, so please be patient with it. 
+>We are working on a systematic validation.
+
---
+++ setup.py
@@ -0,0 +1,43 @@
+
+import os
+from glob import glob
+from setuptools import setup, find_packages
+
+exec(open("mapillm/version.py").read())
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="MAPI_LLM",
+    version=__version__,
+    description="A Python package for the MAPI_LLM project",
+    author="Mayk Caldas",
+    author_email="maykcaldas@gmail.edu",
+    url="https://github.com/maykcaldas/MAPI_LLM",
+    license="MIT",
+    packages=['mapillm'],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "openai",
+        "langchain",
+        "mp_api",
+        "request",
+        "gpt_index",
+        "gradio",
+        "rdkit",
+        "transformers",
+        "pymatgen",
+        "faiss-cpu",
+        "reaction-network",
+    ],
+    test_suite="tests",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
